ab465e621e43f723747121ba8b6403bbbfa4b3e6
shahid-penoft <mohammed.shahid@penoft.com>
2026-03-23 09:21:47 +0530

    rate limit set

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -6,7 +6,7 @@ import rateLimit from 'express-rate-limit';
  */
 export const apiLimiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 1 minute
-    max: 100, 
+    max: 100,
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     message: {
