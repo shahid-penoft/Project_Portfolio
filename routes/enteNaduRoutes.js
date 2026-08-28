558d18548fc69d74647cc9bfcffb282e87b5f2a9
shahid-penoft <mohammed.shahid@penoft.com>
2026-08-28 18:01:03 +0530

    ente nadu route fix

--- routes/enteNaduRoutes.js
+++ routes/enteNaduRoutes.js
@@ -6,15 +6,16 @@ import { uploadIcon, uploadImage, runMulter } from '../configs/multerS3.js';
 const router = express.Router();
 
 // Public
-router.get('/', getCards);
 router.get('/section', getSectionMeta);
+router.get('/', getCards);
 
-// Protected
-router.post('/', verifyToken, uploadIcon, createCard);
-router.put('/:id', verifyToken, uploadIcon, updateCard);
-router.put('/:id/promote', verifyToken, promoteCard);
-router.delete('/:id', verifyToken, deleteCard);
+// Protected - specific sub-routes before parameterized /:id routes
 router.put('/section', verifyToken, updateSectionMeta);
 router.post('/section/upload', verifyToken, uploadSectionImage);
+
+router.post('/', verifyToken, uploadIcon, createCard);
+router.put('/:id/promote', verifyToken, promoteCard);
+router.put('/:id', verifyToken, uploadIcon, updateCard);
+router.delete('/:id', verifyToken, deleteCard);
 
 export default router;
